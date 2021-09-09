fc2a59684b3996e91cc4eca97cb6b627550ba852
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-09 18:14:30 -0500

    added career

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-//Name
+//Name needs to be capped in on screen not in code
 //Career/Field
 //Short Desc
 //Interests
 //Past positions including company/place ,name ,title, short desc of duties
 //Skills "BAM" = cool
+
+console.log('Name: Quincy Stafford')
+console.log('Career :Full Stack Development Student' )
